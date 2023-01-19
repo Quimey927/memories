9272209fbcd9dbb43a535f8d17183ac796a9c020
Quimey <quimeymata@gmail.com>
2023-01-19 10:27:04 -0300

    Removes google auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -9,17 +8,15 @@ import Auth from './components/Auth/Auth';
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <Container maxWidth="lg">
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/auth" exact component={Auth} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
